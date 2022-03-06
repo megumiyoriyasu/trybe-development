ead2de42addca6ae1385f218af8b5bb7618fe4c5
Megumi <megumi.yoriyasu@outlook.com>
2022-03-06 12:44:09 -0300

    finaliza o exercicio 3 do dia 5.3

--- 01-fundamentos/b05-javascript_dom_eventos_e_web_storage/d03-javascript_eventos/exercícios/script.js
+++ 01-fundamentos/b05-javascript_dom_eventos_e_web_storage/d03-javascript_eventos/exercícios/script.js
@@ -13,7 +13,10 @@ function createDaysOfTheWeek() {
 
 createDaysOfTheWeek();
 
-// Exercício 1
+// Desenvolva uma função que crie dinamicamente cada dia do calendário e os adicione como filhos/filhas da tag <ul> com ID "days".
+// Os dias devem estar contidos em uma tag <li> , e todos devem ter a classe day.
+// Os dias 24, 25 e 31 são feriados e, além da classe day , devem conter também a classe holiday.
+// Os dias 4, 11, 18 e 25 são Sexta-feira. Eles devem conter a classe day e a classe friday.
 
 function createDays() {
   const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
@@ -35,19 +38,36 @@ function createDays() {
 }
 createDays();
 
-// Exercício 2
+// Implemente uma função que receba como parâmetro a string "Feriados" e crie dinamicamente um botão com o nome "Feriados".
+// Adicione a este botão a ID "btn-holiday".
+// Adicione este botão como filho/filha da tag <div> com classe "buttons-container".
 
 function holidaysButton (buttonName) {
   const divButton = document.querySelector('.buttons-container');
   let button = document.createElement('button');
-  let buttonID = 'btn-holiday';
-  button.id = buttonID;
+  button.id = 'btn-holiday';
   button.innerText = buttonName;
   divButton.appendChild(button);
 }
 holidaysButton('Feriados');
 
-// Exercício 3
+// Implemente uma função que adicione ao botão "Feriados" um evento de "click" que muda a cor de fundo dos dias que possuem a classe "holiday".
+// É interessante que este botão possua também a lógica inversa. Ao ser clicado novamente ele retorna à configuração inicial com a cor "rgb(238,238,238)".
+
+const buttonHoliday = document.querySelector('#btn-holiday');
+
+function selectHoliday () {
+  const holidayDays = document.querySelectorAll('.holiday');
+  for (let x = 0; x < holidayDays.length; x += 1) {
+    let currentItem = holidayDays[x];
+    if (currentItem.style.backgroundColor != 'rgb(238, 238, 238)') {
+      currentItem.style.backgroundColor = 'rgb(238, 238, 238)';
+    } else if (currentItem.style.backgroundColor == 'rgb(238, 238, 238)') {
+      currentItem.style.backgroundColor = 'rgb(199, 199, 199)';
+    }
+  }
+}
+buttonHoliday.addEventListener('click', selectHoliday);
 
 // Exercício 4
 
