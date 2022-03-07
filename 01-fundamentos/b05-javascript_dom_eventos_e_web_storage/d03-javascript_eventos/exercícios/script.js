2c18c63ab06414ad907636eaf5f07b27e744cf54
Megumi <megumi.yoriyasu@outlook.com>
2022-03-07 00:25:06 -0300

    finaliza dia 5.3

--- 01-fundamentos/b05-javascript_dom_eventos_e_web_storage/d03-javascript_eventos/exercícios/script.js
+++ 01-fundamentos/b05-javascript_dom_eventos_e_web_storage/d03-javascript_eventos/exercícios/script.js
@@ -131,21 +131,20 @@ function addTask () {
   taskElement.innerHTML = input.value + '<br>';
   divTask.appendChild(taskElement);
 }
-input.addEventListener('change', addTask);
 
 // Implemente uma função que adiciona uma legenda com cor para a tarefa criada no exercício anterior. Esta função deverá receber como parâmetro uma string ("cor") e criar dinamicamente um elemento de tag <div> com a classe task .
 // O parâmetro cor deverá ser utilizado como cor de fundo da <div> criada.
 // O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks".
 
-function backColor (color) {
+function backColor () {
   const divTask = document.createElement('div');
   const divMom = document.querySelector('.my-tasks');
   divTask.className = 'task';
-  divTask.style.backgroundColor = color;
+  divTask.style.backgroundColor = 'white';
   divMom.appendChild(divTask);
 }
-backColor('black');
-
+input.addEventListener('change', backColor);
+input.addEventListener('change', addTask);
 
 // Implemente uma função que adiciona um evento que, ao clicar no elemento com a tag <div> referente a cor da sua tarefa, atribua a este elemento a classe task selected , ou seja, quando sua tarefa possuir a classe task selected , ela estará selecionada.
 // Ao clicar novamente no elemento, a sua classe deverá voltar a ser somente task , ou seja, esta tarefa está deixando de ser uma tarefa selecionada.
@@ -163,5 +162,24 @@ function addEvent () {
 }
 addEvent();
 
-// Exercício 10
+// Implemente uma função que adiciona um evento que, ao clicar em um dia do mês no calendário, atribua a este dia a cor da legenda da sua tarefa selecionada.
+// Ao clicar novamente no dia com a cor da legenda, a sua cor deverá voltar à configuração inicial rgb(119,119,119).
+// (retirado do gabarito)
 
+function setDayColor() {
+  let selectedTask = document.getElementsByClassName('task selected');
+  let days = document.querySelector('#days');
+  let taskDiv = document.querySelector('.task');
+  let taskColor = taskDiv.style.backgroundColor;
+  
+  days.addEventListener('click', function(event){
+    let eventTargetColor = event.target.style.color;
+    if (selectedTask.length > 0 && eventTargetColor !== taskColor) {
+      let color = selectedTask[0].style.backgroundColor;
+      event.target.style.color = color;
+    } else if (eventTargetColor === taskColor && selectedTask.length !== 0) {
+      event.target.style.color = 'rgb(119,119,119)';
+    }
+  });
+};
+setDayColor();
