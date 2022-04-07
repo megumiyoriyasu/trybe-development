932326f5cd54256fb145323380018769137ac15a
Megumi <megumi.yoriyasu@outlook.com>
2022-04-07 13:16:39 -0300

    adicionado o prevent default ao link

--- 01-fundamentos/b06-html_css_forms_flexbox_responsivo/d01-html_css_forms/part4.js
+++ 01-fundamentos/b06-html_css_forms_flexbox_responsivo/d01-html_css_forms/part4.js
@@ -2,3 +2,9 @@
 const INPUT_TEXT = document.querySelector("#input-text");
 const INPUT_CHECKBOX = document.querySelector("#input-checkbox");
 const HREF_LINK = document.querySelector("#href");
+
+function cancelLink(event) {
+  event.preventDefault();
+}
+
+HREF_LINK.addEventListener('click', cancelLink)
