148992d711aa7129af0e0a286e746b082e715f56
Megumi <megumi.yoriyasu@outlook.com>
2022-04-08 16:39:01 -0300

    adicionado texto do bonus

--- 01-fundamentos/b06-html_css_forms_flexbox_responsivo/d01-html_css_forms/exercícios/trybetrip.js
+++ 01-fundamentos/b06-html_css_forms_flexbox_responsivo/d01-html_css_forms/exercícios/trybetrip.js
@@ -1,7 +1,14 @@
 const btnSubmit = document.getElementById('submit');
 
 // Interrompa o comportamento padrão do botão submit utilizando o método preventDefault(). Nossa amiga Carol Silva nos contou um pouco sobre como fazer isso, lembra?
+
 function disabledSend(event) {
   event.preventDefault();
 }
 btnSubmit.addEventListener('click', disabledSend);
+
+// Bônus
+
+// A TrybeTrip precisa muito de fotos para divulgar seus concursos. Tendo isso em mente, faça com que somente quem autorizar o uso de imagens possa enviar suas informações.
+
+// Faça a validação dos campos com limite de caracteres. Caso não estejam dentro do esperado ao clicar no botão de submit, um alerta deve ser mostrado com a mensagem: 'Dados Inválidos'. Caso contrário, a mensagem 'Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.' deverá aparecer na tela.
