5b3e3462ffe143999615c6ccfc201b02e65f5154
Megumi <megumi.yoriyasu@outlook.com>
2022-04-18 02:38:12 -0300

    adicionadoo exercicio 2 da parte 1 do dia 7.1

--- 01-fundamentos/b07-introducao_a_js_es6_e_testes_unitarios/d01-js_es6_let_const_arrow_functions_e_template_literals/exercicios/exercicio1.js
+++ 01-fundamentos/b07-introducao_a_js_es6_e_testes_unitarios/d01-js_es6_let_const_arrow_functions_e_template_literals/exercicios/exercicio1.js
@@ -25,3 +25,9 @@ const testingScope = (escopo) => {
 
 testingScope(true);
 testingScope(false);
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+// Seu código aqui.
+
+console.log(oddsAndEvens); // será necessário alterar essa linha 😉
