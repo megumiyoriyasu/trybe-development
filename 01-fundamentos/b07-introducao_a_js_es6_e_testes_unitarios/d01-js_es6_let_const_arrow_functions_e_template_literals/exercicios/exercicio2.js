766410cf7ad1c3b54413df10a0d5bb9d81879fc4
Megumi <megumi.yoriyasu@outlook.com>
2022-04-18 16:49:17 -0300

    finaliza exercicio 2 da parte 2 do dia 7.1

--- 01-fundamentos/b07-introducao_a_js_es6_e_testes_unitarios/d01-js_es6_let_const_arrow_functions_e_template_literals/exercicios/exercicio2.js
+++ 01-fundamentos/b07-introducao_a_js_es6_e_testes_unitarios/d01-js_es6_let_const_arrow_functions_e_template_literals/exercicios/exercicio2.js
@@ -6,3 +6,17 @@ const factorial = (numb) => {
   return factorialResult;
 }
 console.log(factorial(3));
+
+const longestWord = (string) => {
+  let stringArray = string.split(' ');
+  let bigLength = 0;
+  let bigWord = '';
+  for (let word of stringArray) {
+    if (word.length > bigLength) {
+      bigLength = word.length;
+      bigWord = word;
+    }
+  }
+  return bigWord
+}
+console.log(longestWord('Antonio foi ao banheiro e nao sabemos o que aconteceu'));
