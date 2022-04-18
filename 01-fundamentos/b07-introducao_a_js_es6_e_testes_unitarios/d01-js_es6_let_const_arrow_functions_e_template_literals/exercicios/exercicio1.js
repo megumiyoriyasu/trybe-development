62a2ca290ca1f911af6349c4b69e65669ab3b5d6
Megumi <megumi.yoriyasu@outlook.com>
2022-04-18 03:00:50 -0300

    finaliza exercicios da parte 1 do dia 7.1

--- 01-fundamentos/b07-introducao_a_js_es6_e_testes_unitarios/d01-js_es6_let_const_arrow_functions_e_template_literals/exercicios/exercicio1.js
+++ 01-fundamentos/b07-introducao_a_js_es6_e_testes_unitarios/d01-js_es6_let_const_arrow_functions_e_template_literals/exercicios/exercicio1.js
@@ -1,17 +1,3 @@
-// function testingScope(escopo) {
-// 	if (escopo === true) {
-// 		var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-// 		ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
-// 		console.log(ifScope);
-// 	} else {
-// 		var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-// 		console.log(elseScope);
-// 	}
-// 	console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
-// }
-
-// testingScope(true);
-
 const testingScope = (escopo) => {
 	if (escopo === true) {
 		let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
@@ -21,6 +7,7 @@ const testingScope = (escopo) => {
 		let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
 		console.log(elseScope);
 	}
+// 	console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
 }
 
 testingScope(true);
@@ -28,6 +15,6 @@ testingScope(false);
 
 const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
-// Seu código aqui.
+const sortOddsAndEvens = (array) => array.sort((a, b) => a-b);
 
-console.log(oddsAndEvens); // será necessário alterar essa linha 😉
+console.log(sortOddsAndEvens(oddsAndEvens)); // será necessário alterar essa linha 😉
