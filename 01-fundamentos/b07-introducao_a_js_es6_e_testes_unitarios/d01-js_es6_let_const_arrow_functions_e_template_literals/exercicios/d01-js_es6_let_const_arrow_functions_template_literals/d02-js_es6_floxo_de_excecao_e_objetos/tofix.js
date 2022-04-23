872d62fe9970cbe5cdf601220d04b4456e04deb9
Megumi <megumi.yoriyasu@outlook.com>
2022-04-23 20:10:40 -0300

    finalizado exercicio da parte II do dia 7.2

--- 01-fundamentos/b07-introducao_a_js_es6_e_testes_unitarios/d01-js_es6_let_const_arrow_functions_e_template_literals/exercicios/d01-js_es6_let_const_arrow_functions_template_literals/d02-js_es6_floxo_de_excecao_e_objetos/tofix.js
+++ 01-fundamentos/b07-introducao_a_js_es6_e_testes_unitarios/d01-js_es6_let_const_arrow_functions_e_template_literals/exercicios/d01-js_es6_let_const_arrow_functions_template_literals/d02-js_es6_floxo_de_excecao_e_objetos/tofix.js
@@ -5,3 +5,28 @@ const makeObject = (object, key, value) => {
 	console.log(object);
 }
 makeObject('user', 'nickname', 'naido');
+
+// Part II
+const student1 = {
+  Html: 'Muito Bom',
+  Css: 'Bom',
+  JavaScript: 'Ótimo',
+  SoftSkills: 'Ótimo',
+};
+
+const student2 = {
+  Html: 'Bom',
+  Css: 'Ótimo',
+  JavaScript: 'Ruim',
+  SoftSkills: 'Ótimo',
+  Git: 'Bom', // chave adicionada
+};
+
+const studentSkills = (student) => {
+  const skills = Object.keys(student);
+  for (const x in skills) {
+    console.log(`${skills[x]}, Nível: ${student[skills[x]]}`);
+  }
+};
+studentSkills(student1);
+studentSkills(student2);
