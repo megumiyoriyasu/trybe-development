6eae04247f0a84584f067f390cf17e0801c0df31
Megumi <megumi.yoriyasu@outlook.com>
2022-04-24 21:42:04 -0300

    tratamento do erro de input vazio

--- 01-fundamentos/b07-introducao_a_js_es6_e_testes_unitarios/d01-js_es6_let_const_arrow_functions_e_template_literals/exercicios/d01-js_es6_let_const_arrow_functions_template_literals/d02-js_es6_floxo_de_excecao_e_objetos/exercicios/part1.js
+++ 01-fundamentos/b07-introducao_a_js_es6_e_testes_unitarios/d01-js_es6_let_const_arrow_functions_e_template_literals/exercicios/d01-js_es6_let_const_arrow_functions_template_literals/d02-js_es6_floxo_de_excecao_e_objetos/exercicios/part1.js
@@ -1,11 +1,35 @@
-function sum() {
+// 1 - Crie um erro personalizado que será chamado se a pessoa usuária digitar apenas um número.
+//   Tente executar a aplicação com um dos valores em branco. Notou que o retorno não é muito descritivo?
+//   Utilize o throw new Error e o bloco try/catch .
+//   Imprima o erro no parágrafo com id result , para que a pessoa usuária saiba o que aconteceu. Lembre-se de usar erros descritivos!
+//   Evite funções que tenham muitas responsabilidades distintas.
+// 2 - Agora adicione uma outra exceção, caso a pessoa usuária digite um valor que não seja numérico.
+//   Você pode fazer essa verificação utilizando a função isNan().
+// 3 - Você se lembrou de limpar os inputs após o clique do botão? Teve que repetir código para isso? Que tal refatorar sua função utilizando o finally?
+
+
+const checkInputs = (input1, input2) => {
+  if (input1 === '' || input2 === '') {
+    throw new Error ('São necessários dois valores para a realizar a soma.');
+  }
+}
+
+const sum = () => {
   const value1 = document.getElementById('value1').value;
   const value2 = document.getElementById('value2').value;
-  const result = parseInt(value1) + parseInt(value2);
-  document.getElementById('result').innerHTML = `Resultado: ${result}`;
-  document.getElementById('value1').value = '';
-  document.getElementById('value2').value = '';
+  const resultMessage = document.getElementById('result');
+  try {
+    checkInputs(value1, value2);
+    const result = parseInt(value1) + parseInt(value2);
+    return resultMessage.innerHTML = `Resultado: ${result}`;
+    document.getElementById('value1').value = '';
+    document.getElementById('value2').value = '';
+  } catch (error) {
+    resultMessage.innerHTML = `${error.message}`;
+    resultMessage.style.color = 'red';
+  }
 }
+
 window.onload = () => {
   const button = document.getElementById('button');
   button.addEventListener('click', sum);
