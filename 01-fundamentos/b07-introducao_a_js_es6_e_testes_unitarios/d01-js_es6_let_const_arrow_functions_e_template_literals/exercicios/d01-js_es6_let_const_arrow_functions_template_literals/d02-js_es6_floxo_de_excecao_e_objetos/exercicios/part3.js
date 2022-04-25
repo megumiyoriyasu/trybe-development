37fea76760f88831220041c0b26d2354df3d1031
Megumi <megumi.yoriyasu@outlook.com>
2022-04-25 12:24:29 -0300

    criado objeto que agrupa todos objetos

--- 01-fundamentos/b07-introducao_a_js_es6_e_testes_unitarios/d01-js_es6_let_const_arrow_functions_e_template_literals/exercicios/d01-js_es6_let_const_arrow_functions_template_literals/d02-js_es6_floxo_de_excecao_e_objetos/exercicios/part3.js
+++ 01-fundamentos/b07-introducao_a_js_es6_e_testes_unitarios/d01-js_es6_let_const_arrow_functions_e_template_literals/exercicios/d01-js_es6_let_const_arrow_functions_template_literals/d02-js_es6_floxo_de_excecao_e_objetos/exercicios/part3.js
@@ -20,11 +20,11 @@ const lesson3 = {
 
 // Crie uma função para adicionar o turno da noite na lesson2. Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
 
-const addKey = (object, key, value) => {
+const addKeyValue = (object, key, value) => {
   object[key] = value;
   console.log(object);
 }
-addKey(lesson2, 'turno', 'noite');
+addKeyValue(lesson2, 'turno', 'noite');
 
 // Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
 
@@ -39,6 +39,10 @@ const countKeys = (object) => listKeys(object).length;
 const listValues = (object) => Object.values(object);
 
 // Crie um objeto de nome allLessons , que deve agrupar todas as aulas através do Object.assign . Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1 , lesson2 e lesson3.
+
+const allLessons = Object.assign({}, {lesson1, lesson2, lesson3});
+console.log(allLessons);
+
 // Usando o objeto criado no exercício 5, crie uma função que retorne o número total de estudantes em todas as aulas.
 // Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto.
 // Crie uma função que verifique se o par (chave / valor) existe na função. Essa função deve possuir três parâmetros, sendo eles: o objeto, o nome da chave e o valor da chave.
