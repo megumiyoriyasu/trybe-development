d35c9a7b4aff641b5cddbc757c19c0bf8ba4f72d
Megumi <megumi.yoriyasu@outlook.com>
2022-05-03 07:41:11 -0300

    cria a funções e os testes mapString, encode e decode

--- 01-fundamentos/b07-introducao_a_js_es6_e_testes_unitarios/d03-primeiros_passos_em_jest/sum.js
+++ 01-fundamentos/b07-introducao_a_js_es6_e_testes_unitarios/d03-primeiros_passos_em_jest/sum.js
@@ -16,11 +16,49 @@ function myRemove(arr, item) {
 }
 
 function myFizzBuzz(num) {
-  if (typeof num !== 'number') return false;
-  if (num % 3 === 0 && num % 5 === 0) return 'fizzbuzz';
-  if (num % 3 === 0) return 'fizz';
-  if (num % 5 === 0) return 'buzz';
-  return num;
+	if (typeof num !== 'number') return false;
+	if (num % 3 === 0 && num % 5 === 0) return 'fizzbuzz';
+	if (num % 3 === 0) return 'fizz';
+	if (num % 5 === 0) return 'buzz';
+	return num;
 }
 
-module.exports = { sum, myRemove, myFizzBuzz };
+function mapString(objectMap, string) {
+	const splitString = string.split('');
+	const mappedArray = [];
+
+	for (let index = 0; index < splitString.length; index += 1) {
+		const character = splitString[index];
+		const mappedValue = objectMap[character];
+		if (mappedValue) {
+			mappedArray.push(mappedValue);
+		} else {
+			mappedArray.push(character);
+		}
+	}
+	return mappedArray.join('');
+}
+
+function encode(string) {
+	const map = {
+		a: 1,
+		e: 2,
+		i: 3,
+		o: 4,
+		u: 5,
+	};
+	return mapString(map, string);
+}
+
+function decode(string) {
+	const map = {
+		1: 'a',
+		2: 'e',
+		3: 'i',
+		4: 'o',
+		5: 'u',
+	};
+	return mapString(map, string);
+}
+
+module.exports = { sum, myRemove, myFizzBuzz, mapString, encode, decode };
--- 01-fundamentos/b07-introducao_a_js_es6_e_testes_unitarios/d03-primeiros_passos_em_jest/sum.test.js
+++ 01-fundamentos/b07-introducao_a_js_es6_e_testes_unitarios/d03-primeiros_passos_em_jest/sum.test.js
@@ -1,4 +1,4 @@
-const {sum, myRemove, myFizzBuzz} = require('./sum');
+const { sum, myRemove, myFizzBuzz, mapString, encode, decode } = require('./sum');
 
 describe('Testes relacionados a função sum', () => {
   it('Verifica retorno da soma dos parâmetros', () => {
@@ -26,5 +26,42 @@ describe('Testes relacionados a função myFizzBuzz', () => {
     expect(myFizzBuzz(20)).toBe('buzz');
     expect(myFizzBuzz(11)).toBe(11);
     expect(myFizzBuzz('Nala')).toBeFalsy();
+  });
+})
+
+describe('Testes relacionados as funções encode e decode', () => {
+  it('Verifica a existência das funções encode e decode', () => {
+    expect(typeof encode).toEqual('function');
+    expect(typeof decode).toEqual('function');
+  });
+  it('Verifica se vogais são convertidas na codificação', () => {
+    expect(encode('a')).toBe('1');
+    expect(encode('e')).toBe('2');
+    expect(encode('i')).toBe('3');
+    expect(encode('o')).toBe('4');
+    expect(encode('u')).toBe('5');
+  });
+  it('Verifica se vogais são convertidas na decodificação', () => {
+    expect(decode('1')).toBe('a');
+    expect(decode('2')).toBe('e');
+    expect(decode('3')).toBe('i');
+    expect(decode('4')).toBe('o');
+    expect(decode('5')).toBe('u');
+  });
+  it('Verifica se somente as vogais sofrem alteração na codificação e decodificação', () => {
+    expect(encode('cachorro')).toBe('c1ch4rr4');
+    expect(encode('beluga')).toBe('b2l5g1');
+    expect(encode('girafa')).toBe('g3r1f1');
+    expect(decode('z2br1')).toBe('zebra');
+    expect(decode('2squ3l4')).toBe('esquilo');
+    expect(decode('p5m1')).toBe('puma');
+  });
+  it('Verifica se a string de saída contém a mesma quantidade de caracteres que a de entrada', () => {
+    expect(encode('lobo').length).toBe(4);
+    expect(encode('impala').length).toBe(6);
+    expect(encode('porco').length).toBe(5);
+    expect(decode('t3gr2').length).toBe(5);
+    expect(decode('m1ng5st4').length).toBe(8);
+    expect(decode('l1g4st1').length).toBe(7);
   });
 })
